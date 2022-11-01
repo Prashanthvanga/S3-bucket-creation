1c4046a2c4f1da57e2b331a7b5a8640f6e818e26
madhu6793 <114066577+madhu6793@users.noreply.github.com>
2022-11-01 16:58:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,13 +10,13 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-west-1"
-  access_key = "AKIAYDSN7W63BBONGTFD"
-  secret_key = "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"
+  access_key = " "
+  secret_key = " "
 }
 
 provider "aws"  {
   alias  = "usw2"
   region = "us-west-2"
-  access_key = "AKIAYDSN7W63BBONGTFD"
-  secret_key = "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"
+  access_key = " "
+  secret_key = " "
 }
